7f570108b648c3bf67844238bbd851403524f583
Luke Hewitt <lhewitt@protonmail.xom>
2024-06-15 15:17:06 -0700

    Call out to OpenAI API

--- tasks.py
+++ tasks.py
@@ -3,15 +3,16 @@ from celery import Celery
 from celery.utils.log import get_task_logger
 
 from models import AddResult
+from api import generate
 
 app = Celery('tasks', broker=os.getenv("CELERY_BROKER_URL"))
 logger = get_task_logger(__name__)
-
-
 
 @app.task
 def add(x, y):
     logger.info(f'Adding {x} + {y}')
     result = x + y
     AddResult.create(x=x, y=y, result=result)
-    return result
+
+    bar = generate.foo()
+    return bar
